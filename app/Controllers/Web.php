c44b6d3c2658ac949f0e783f22c4082bcef74b50
danuja-dilanka <dilanka.rajapakshe1@gmail.com>
2023-06-01 21:02:03 +0530

    update

--- app/Controllers/Web.php
+++ app/Controllers/Web.php
@@ -33,13 +33,15 @@ class Web extends BaseController {
             foreach ($available_loans as $key => $value) {
                 if ($value->status == 1) {
                     $avoid_data = $this->thisModel->get_loan_pay_data_summary(['loan' => $value->id]);
+                    $avoid_data["p_periods"] = $value->loan_period - $value->paid_period;
+                    $avoid_data["p_charge"] = round($value->last_amount / $value->paid_period);
                     $avoid_data->loan_amount = $value->last_amount;
                     $avoid_data->id = $value->id;
                     break;
                 }
             }
 
-            if (count($avoid_data) > 0) {
+            if (isset($avoid_data->id)) {
                 return view('loan_app_stage2', ['lng' => $lng, 'prev_loan' => $avoid_data]);
             } else {
                 if ($lng == "si") {
