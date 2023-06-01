84ed795aecf1b19ee1f2075ef1073023c93eeb4f
danuja-dilanka <dilanka.rajapakshe1@gmail.com>
2023-06-02 00:10:15 +0530

    Update loan_app_stage2.php

--- app/Views/loan_app_stage2.php
+++ app/Views/loan_app_stage2.php
@@ -28,7 +28,6 @@
                                 <div class="card-body">
                                     <?= form_open_multipart(base_url("loan_check/$lng"), array('data-parsley-validate' => 'true')); ?>
                                     <img class="logo" src="<?= base_url() ?>public/uploads/media/logo.png">
-                                    <h5 class="text-center py-4"><?= $lng == 'si' ? "හැදුනුම්පත් අංකය" : "Enter NIC" ?></h5> 
                                     <?php if (count(validation_errors()) > 0) { ?>
                                         <div class="alert alert-danger">
                                             <?= validation_list_errors() ?>
@@ -50,8 +49,8 @@
                                                     $p_periods = $prev_loan->p_periods;
                                                     for ($i = 0; $i < $prev_loan->p_periods; $i++) { ?>
                                                         <tr>
-                                                            <td><?= $i +1 ?></td>
-                                                            <td><?= $p_periods++ ?></td>
+                                                            <td><?= $i +1 ?>&nbsp;<input type="checkbox" name="p_periods[]" /></td>
+                                                            <td>Loan Period <?= $p_periods++ ?></td>
                                                             <td>LKR. <?= number_format($prev_loan->p_charge, 2, ".", ",") ?></td>
                                                         </tr>
                                                     <?php } ?>
@@ -59,6 +58,7 @@
                                             </table>
                                         </div>
                                     <?php } ?>
+                                    <h5 class="text-center py-4"><?= $lng == 'si' ? "හැදුනුම්පත් අංකය" : "Enter NIC" ?></h5> 
                                     <div class="form-group row">
                                         <div class="col-md-12">
                                             <input name="nic" id="text" placeholder="<?= $lng == 'si' ? "හැදුනුම්පත් අංකය" : "Enter NIC" ?>" type="text" class="form-control" minlength="10" maxlength="12" required><br>
