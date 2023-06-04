1332ef592e78a57526c0dd2471022097165ec2f9
danuja-dilanka <dilanka.rajapakshe1@gmail.com>
2023-06-04 11:48:14 +0530

    update

--- app/Views/_loan/_loan_group/_list.php
+++ app/Views/_loan/_loan_group/_list.php
@@ -8,7 +8,7 @@
             
         </div>
         <div class="col-md-1">
-            <?php create_link("loan", "loan_group", ["group_data", "add"], "+ New", "h6") ?>
+            <?= create_link("loan", "loan_group", ["group_data", "add"], "+ New", "h6") ?>
         </div>
     </div>
     <table id="dt_tb" class="table" data-action="loan_groups" style="width:100%">
