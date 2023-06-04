0b204d48536cb4565ee92103a289e41aadae958f
danuja-dilanka <dilanka.rajapakshe1@gmail.com>
2023-06-04 21:16:40 +0530

    sweet model

--- app/Controllers/View_data.php
+++ app/Controllers/View_data.php
@@ -32,6 +32,7 @@ class View_data extends BaseController {
                 $value->address,
                 $value->photo,
                 (has_permission("member", "edit") ? "<a href='" . base_url("member/mem/") . $key_enc . "' class='btn btn-sm btn-primary'>Edit</a>&nbsp;" : "") .
+                ("<button type='button' onclick='open_rating(this)' data-key='".$key_enc."' data-type='rating' class='btn btn-sm btn-primary'>Rate</button>&nbsp;") .
                 (has_permission("member", "delete") ? "<a href='#' data-id='" . base_url("member/del_mem/") . $key_enc . "' class='btn btn-sm btn-danger confirm_red_btn'>Delete</a>" : "")
             ];
         }
---
+++ public/assets/js/sweet_model.js
@@ -0,0 +1,17 @@
+var rating_template = "<div class='col-sm-12'><div class='col-sm-6'><input class'form-group' placeholder='Description'/></div></div>";
+var templates = {"rating": ["Ratings", rating_template]};
+
+function get_template(type, key) {
+    var mod_template = templates[type];
+    mod_template[1] = mod_template[1].replace(/KEY/g, key);
+    return mod_template;
+}
+
+function open_rating(element) {
+    var template = get_template(element.data('type'), element.data('key'));
+    $.sweetModal({
+        title: template[0],
+        content: template[1],
+        theme: $.sweetModal.THEME_DARK
+    });
+}
