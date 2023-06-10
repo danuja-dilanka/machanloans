c732f32513e08432fde9ed2e091effafd4f57a36
danuja-dilanka <dilanka.rajapakshe1@gmail.com>
2023-06-11 00:16:44 +0530

    Update _member.php

--- app/Views/_member/_member.php
+++ app/Views/_member/_member.php
@@ -77,7 +77,7 @@
                             <?= render_input('password', 'Password', '', 'password'); ?>
                         </div>
                         <div class="col-md-12">
-                            <?= render_custom_select("status", ["Active", "In Active"], "Status", isset($data) ? $data->status : '') ?>
+                            <?= render_custom_select("status", ["Active", "In Active"], "Status", isset($data) ? $data->status : 1) ?>
                         </div>
                     </div>
                 </div>
