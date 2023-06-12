10dd6eb59a732c84f68875f1a6c20654e3f19bce
danuja-dilanka <dilanka.rajapakshe1@gmail.com>
2023-06-12 18:12:25 +0530

    Update common_func.js

--- public/assets/js/common_func.js
+++ public/assets/js/common_func.js
@@ -1,14 +1,10 @@
-function checkJSFileExists(filePath) {
-    return fetch(filePath).then(response => {
-        if (response.status === 200) {
-            return true;
-        } else {
-            return false;
-        }
-    }).catch(() => {
-        return false;
-    });
+function file_exist(url) {
+    var http = new XMLHttpRequest();
+    http.open('HEAD', url, false);
+    http.send();
+    return http.status != 404;
 }
+
 $(document).ready(function () {
     const currentUrl = window.location.href;
     const parts = currentUrl.split('/');
@@ -18,17 +14,21 @@ $(document).ready(function () {
     $.getScript(BASE_URL + "public/assets/js/ajax_api/setting_users.js");
     $.getScript(BASE_URL + "public/assets/js/ajax_api/setting_user_types.js");
     $.getScript(BASE_URL + "public/assets/js/post_load/settings.js");
-    $.getScript(BASE_URL + "public/assets/js/post_load/dashboard.js");
 
     if (tot_parts > 1) {
         var dec = 1;
         if (SECRET_ID != "") {
             dec = dec + 1;
         }
-        const jsFilePath = BASE_URL + "public/assets/js/post_load/" + parts[tot_parts - (dec + 1)] + "_" + parts[tot_parts - dec] + ".js";
-        checkJSFileExists(jsFilePath).then(exists => {
+        var jsFilePath = BASE_URL + "public/assets/js/post_load/" + parts[tot_parts - (dec + 1)] + "_" + parts[tot_parts - dec] + ".js";
+        if (file_exist(jsFilePath)) {
             $.getScript(jsFilePath);
-        });
+        } else {
+            jsFilePath = BASE_URL + "public/assets/js/post_load/" + parts[tot_parts - dec] + ".js";
+            if (file_exist(jsFilePath)) {
+                $.getScript(jsFilePath);
+            }
+        }
     }
 });
 
