dfd9030323d16adb6e9238519da583f9d8b845b0
danuja-dilanka <dilanka.rajapakshe1@gmail.com>
2023-06-13 21:33:13 +0530

    Update View_data.php

--- app/Controllers/View_data.php
+++ app/Controllers/View_data.php
@@ -34,7 +34,7 @@ class View_data extends BaseController {
             
             $data[] = [
                 $key,
-                $value->photo != "" ? "<a href='". base_url("public/images/member/") . $value->photo."' target='_blank'><img src='". base_url("public/images/member/") . $value->photo."' width='150'/></a>" : base_url("public/uploads/profile/") . "default.png",
+                $value->photo != "" ? "<a href='". base_url("public/images/member/") . $value->photo."' target='_blank'><img src='". base_url("public/images/member/") . $value->photo."' width='100'/></a>" : base_url("public/uploads/profile/") . "default.png",
                 $value->first_name,
                 $value->last_name,
                 $value->member_no,
