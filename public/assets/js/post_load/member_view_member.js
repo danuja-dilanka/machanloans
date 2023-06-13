42f48b49299d2f88f1b6b7ca1e0752cb0bcbd9da
danuja-dilanka <dilanka.rajapakshe1@gmail.com>
2023-06-13 20:20:00 +0530

    Update member_view_member.js

--- public/assets/js/post_load/member_view_member.js
+++ public/assets/js/post_load/member_view_member.js
@@ -21,3 +21,23 @@ $(document).on("click", "#send_sms", function () {
     });
 });
 
+$.post(BASE_URL + "api/send_sms", {"user": $("input[name='user']").val().trim(), "message": $("textarea[name='message']").val().trim()}, function (data) {
+        alert(data);
+//        if (data == '1') {
+//            $.toast({
+//                heading: 'Alert',
+//                text: "SMS Sent",
+//                position: 'bottom-right',
+//                showHideTransition: 'slide',
+//                icon: 'success'
+//            });
+//        } else {
+//            $.toast({
+//                heading: 'Alert',
+//                text: "Failed",
+//                position: 'bottom-right',
+//                showHideTransition: 'slide',
+//                icon: 'error'
+//            });
+//        }
+    });
