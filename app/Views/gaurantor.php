f1fbb39921a5df371ea34b3f35d4a6397efec9f2
danuja-dilanka <dilanka.rajapakshe1@gmail.com>
2023-06-15 10:51:58 +0530

    Update gaurantor.php

--- app/Views/gaurantor.php
+++ app/Views/gaurantor.php
@@ -116,6 +116,6 @@
     var BASE_URL = '<?= base_url() ?>';
 </script>
 <script type="text/javascript" src="<?= base_url() ?>public/assets/js/plupload.full.min.js"></script>
-<script src="<?= base_url() ?>public/assets/js/upload.js?v=1"></script>
+<script src="<?= base_url() ?>public/assets/js/upload.js?v=2"></script>
 </body>
 </html>
