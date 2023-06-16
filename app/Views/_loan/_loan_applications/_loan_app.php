b75edb1b757cb7e42665bd636b67f8d3a5cb23df
danuja-dilanka <dilanka.rajapakshe1@gmail.com>
2023-06-16 07:59:01 +0530

    Update _loan_app.php

--- app/Views/_loan/_loan_applications/_loan_app.php
+++ app/Views/_loan/_loan_applications/_loan_app.php
@@ -119,7 +119,7 @@
                             </div>
                             <div class="col-md-4">
                                 <img src="<?= isset($member->photo) && $member->photo != "" ? base_url("public/images/member/") . $member->photo : base_url("public/uploads/profile/") . "default.png" ?>" class="thumb-image-md">
-                                <br><a href="<?= base_url("member/mem/"). encode($member->id) ?>" class="btn btn-primary btn-lg btn-block">Edit Member Details</a>
+                                <a style="margin-top:2%" href="<?= base_url("member/mem/"). encode($member->id) ?>" class="btn btn-primary btn-lg btn-block">Edit Member Details</a>
                             </div>
                         </div>
                         <div class="col-md-12 text-center">
