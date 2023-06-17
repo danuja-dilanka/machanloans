901e7ce7a6b6700d8d3373d9a59c81be9f23b82c
danuja-dilanka <dilanka.rajapakshe1@gmail.com>
2023-06-17 22:23:54 +0530

    Update _loan_app.php

--- app/Views/_loan/_loan_applications/_loan_app.php
+++ app/Views/_loan/_loan_applications/_loan_app.php
@@ -19,7 +19,7 @@
                             <hr>
                         </div>
                         <div class="row col-md-12">
-                            <div class="col-md-9">
+                            <div class="col-md-12">
                                 <nav>
                                     <div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
                                         <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Basic Details</a>
@@ -28,42 +28,50 @@
                                 </nav>
                                 <div class="tab-content py-3 px-3 px-sm-0" id="nav-tabContent">
                                     <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    <label class="control-label">Name</label>
-                                                    <input type="text" class="form-control" value="<?= isset($member) ? $member->first_name . " " . $member->last_name : "" ?>" readonly="">
+                                        <div class="col-md-12 row">
+                                            <div class="col-md-9">
+                                                <div class="row">
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <label class="control-label">Name</label>
+                                                            <input type="text" class="form-control" value="<?= isset($member) ? $member->first_name . " " . $member->last_name : "" ?>" readonly="">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <label class="control-label">Member No</label>
+                                                            <input type="text" class="form-control" value="<?= isset($member) ? $member->member_no : "" ?>" readonly>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <label class="control-label">Phone number</label>
+                                                            <input type="text" class="form-control" value="<?= isset($member) ? $member->mobile : "" ?>" readonly>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <label class="control-label">Email </label>
+                                                            <input type="email" class="form-control" value="<?= isset($member) ? $member->email : "" ?>" readonly>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <label class="control-label">Address on ID</label>
+                                                            <textarea class="form-control" readonly><?= isset($member) ? $member->address : "" ?></textarea>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <label class="control-label">Current Residential Address</label>
+                                                            <textarea class="form-control" readonly><?= isset($member) ? $member->cred_address : "" ?></textarea>
+                                                        </div>
+                                                    </div>
                                                 </div>
                                             </div>
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    <label class="control-label">Member No</label>
-                                                    <input type="text" class="form-control" value="<?= isset($member) ? $member->member_no : "" ?>" readonly>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    <label class="control-label">Phone number</label>
-                                                    <input type="text" class="form-control" value="<?= isset($member) ? $member->mobile : "" ?>" readonly>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    <label class="control-label">Email </label>
-                                                    <input type="email" class="form-control" value="<?= isset($member) ? $member->email : "" ?>" readonly>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    <label class="control-label">Address on ID</label>
-                                                    <textarea class="form-control" readonly><?= isset($member) ? $member->address : "" ?></textarea>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    <label class="control-label">Current Residential Address</label>
-                                                    <textarea class="form-control" readonly><?= isset($member) ? $member->cred_address : "" ?></textarea>
-                                                </div>
+                                            <div class="col-md-3">
+                                                <img src="<?= isset($member->photo) && $member->photo != "" ? base_url("public/images/member/") . $member->photo : base_url("public/uploads/profile/") . "default.png" ?>" class="thumb-image-md" width="250">
+                                                <a style="margin-top:2%" href="<?= base_url("member/mem/") . encode($member->id) ?>" class="btn btn-primary btn-lg btn-block">Edit Member Details</a>
                                             </div>
                                         </div>
                                     </div>
@@ -134,10 +142,6 @@
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-md-3">
-                                <img src="<?= isset($member->photo) && $member->photo != "" ? base_url("public/images/member/") . $member->photo : base_url("public/uploads/profile/") . "default.png" ?>" class="thumb-image-md" width="250">
-                                <a style="margin-top:2%" href="<?= base_url("member/mem/") . encode($member->id) ?>" class="btn btn-primary btn-lg btn-block">Edit Member Details</a>
                             </div>
                         </div>
                         <div class="col-md-12 text-center">
