3ab040467d31b84028736ff935f6e127c102a0be
danuja-dilanka <dilanka.rajapakshe1@gmail.com>
2023-06-29 17:31:49 +0530

    up

---
+++ public/assets/js/post_load/loan_loan_group.js
@@ -0,0 +1,31 @@
+$('#members').selectpicker({
+    liveSearch: true,
+    liveSearchNormalize: true,
+    liveSearchStyle: 'startsWith'
+}).ajaxSelectPicker({
+    ajax: {
+        url: BASE_URL + "/api/members",
+        type: 'POST',
+        data: function () {
+            var query = {
+                search: '{{{q}}}'
+            };
+            return query;
+        }
+    },
+    locale: {
+        emptyTitle: 'Search'
+    },
+    preprocessData: function (data) {
+        var items = [];
+        if (data.hasOwnProperty('items')) {
+            var len = data.items.length;
+            for (var i = 0; i < len; i++) {
+                var curr = data.items[i];
+                items.push({'value': curr.id, 'text': curr.text});
+            }
+        }
+        return items;
+    },
+    preserveSelected: false
+});
