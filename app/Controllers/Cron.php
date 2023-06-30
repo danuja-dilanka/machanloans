cfebc22d68e416b4ead7cf2a857487b271df2c8e
danuja-dilanka <dilanka.rajapakshe1@gmail.com>
2023-06-30 20:57:07 +0530

    Update Cron.php

--- app/Controllers/Cron.php
+++ app/Controllers/Cron.php
@@ -24,7 +24,7 @@ class Cron extends BaseController {
                 if (strlen($birth_tody->mobile) == 9) {
                     $sms_wish = $Member_model->get_sms_wishs_by(["date" => date("Y-m-d"), "mobile" => $birth_tody->mobile]);
                     if (!isset($sms_wish->id)) {
-                        send_sms([$birth_tody->mobile], $wish);
+                        send_sms($birth_tody->mobile, $wish);
                         $Member_model->add_sms_wish([
                             "date" => date("Y-m-d"),
                             "mobile" => $birth_tody->mobile,
