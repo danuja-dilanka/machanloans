23cad8f64fa1d8901697b9c870895403da668fed
danuja-dilanka <dilanka.rajapakshe1@gmail.com>
2023-06-30 20:27:53 +0530

    Update View_data.php

--- app/Controllers/View_data.php
+++ app/Controllers/View_data.php
@@ -71,7 +71,10 @@ class View_data extends BaseController {
         $loans = model('Loan_model')->get_loan_req_data();
         $today = date('Y-m-d');
         foreach ($loans as $key => $value) {
-            if ($status != "" && preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/", $value->loan_rel_date)) {
+            if ($status != "") {
+                if(!preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/", $value->loan_rel_date)){
+                    continue;
+                }
                 $monthsToAdd = (int) ($value->lp_term_per == 1 ? $value->lp_term : $value->lp_term * 12);
                 $due_date = date('Y-m-d', strtotime($value->loan_rel_date . " +$monthsToAdd months"));
 
@@ -108,7 +111,7 @@ class View_data extends BaseController {
             }
 
             $data[] = [
-                $status." - ".$value->id,
+                $value->id,
                 $value->loan_product,
                 $value->full_name != null ? $value->full_name : $value->mem_name,
                 "MPL-" . $value->mem_no,
