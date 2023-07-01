1956de9c6028060f0df747dace678a6720d6a18a
danuja-dilanka <dilanka.rajapakshe1@gmail.com>
2023-07-01 19:20:25 +0530

    Update loan_helper.php

--- app/Helpers/loan_helper.php
+++ app/Helpers/loan_helper.php
@@ -8,53 +8,23 @@ function get_due_loan_periods($loan_id) {
     }
 
     $total = floatval($loan_det->last_amount) + (floatval($loan_det->last_amount) * (floatval($loan_det->lp_int_rate) / 100));
-//        if ($loan_det->lp_term_per == 1) {
-//            $divisor = $loan_det->payment_method == 1 ? $loan_det->lp_term : floatval($loan_det->lp_term) * 4;
-//        } else {
-//            $divisor = $loan_det->payment_method == 1 ? $loan_det->lp_term * 12 : floatval($loan_det->lp_term) * 12 * 4;
-//        }
-
     $divisor = $loan_det->lp_term > 0 ? $loan_det->lp_term : 1;
     $charg_per_period = round($total / $divisor, 2);
 
     $due_dates = [];
     for ($i = 1; $i <= $divisor; $i++) {
-        if ($loan_det->loan_rel_date == "") {
+        if (!preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/", $loan_det->loan_rel_date)) {
             break;
         }
 
         if ($loan_det->lp_term_per == 1 && $loan_det->payment_method == 1) {
-            $monthsToAdd = $i;
-
-            $dateTime = new \DateTime($loan_det->loan_rel_date);
-            $dateTime->add(new \DateInterval("P{$monthsToAdd}M"));
-
-            $newDate = $dateTime->format("Y-m-d");
-            $due_dates[] = $newDate;
+            $due_dates[] = date('Y-m-d', strtotime($loan_det->loan_rel_date . " +" . ($i) . " months"));
         } else if ($loan_det->lp_term_per == 1 && $loan_det->payment_method == 2) {
-            $weeksToAdd = $i * 4;
-
-            $dateTime = new \DateTime($loan_det->loan_rel_date);
-            $dateTime->add(new \DateInterval("P{$weeksToAdd}W"));
-
-            $newDate = $dateTime->format("Y-m-d");
-            $due_dates[] = $newDate;
+            $due_dates[] = date('Y-m-d', strtotime($loan_det->loan_rel_date . " +" . ($i) . " weeks"));
         } else if ($loan_det->lp_term_per == 2 && $loan_det->payment_method == 1) {
-            $monthsToAdd = $i * 12;
-
-            $dateTime = new \DateTime($loan_det->loan_rel_date);
-            $dateTime->add(new \DateInterval("P{$monthsToAdd}M"));
-
-            $newDate = $dateTime->format("Y-m-d");
-            $due_dates[] = $newDate;
+            $due_dates[] = date('Y-m-d', strtotime($loan_det->loan_rel_date . " +" . ($i) . " months"));
         } else if ($loan_det->lp_term_per == 2 && $loan_det->payment_method == 2) {
-            $weeksToAdd = $i * 12 * 4;
-
-            $dateTime = new \DateTime($loan_det->loan_rel_date);
-            $dateTime->add(new \DateInterval("P{$weeksToAdd}W"));
-
-            $newDate = $dateTime->format("Y-m-d");
-            $due_dates[] = $newDate;
+            $due_dates[] = date('Y-m-d', strtotime($loan_det->loan_rel_date . " +" . ($i) . " weeks"));
         }
     }
 
