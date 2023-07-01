4e7c0619be64d37d2921bf239a899afcbf31735e
danuja-dilanka <dilanka.rajapakshe1@gmail.com>
2023-07-01 12:33:55 +0530

    Update sweet_model.js

--- public/assets/js/sweet_model.js
+++ public/assets/js/sweet_model.js
@@ -1,8 +1,9 @@
-var templates = {"rating": ["Ratings", '<div class="row"><div class="rate"><input type="radio" id="star5" name="rate" value="5" class="KEY"/><label for="star5" title="text">5 stars</label><input type="radio" id="star4" name="rate" value="4" class="KEY" /><label for="star4" title="text">4 stars</label><input type="radio" id="star3" name="rate" value="3" class="KEY" /><label for="star3" title="text">3 stars</label><input type="radio" id="star2" name="rate" value="2" class="KEY" /><label for="star2" title="text">2 stars</label><input type="radio" id="star1" name="rate" value="1" class="KEY" /><label for="star1" title="text">1 star</label></div><div class="col-sm-12"></div><div class="col-sm-12"><textarea class="form-control" placeholder="Description" id="des__KEY"></textarea></div><div class="row col-sm-12"><div class="col-sm-10"></div><div class="col-sm-2"><hr><button type="button" class="btn btn-primary" onclick="add_rate(this)" data-id="KEY">Submit</button></div></div></div>']};
+var rating_template = '<div class="row"><div class="rate"><input type="radio" id="star5" name="rate" value="5" class="KEY"/><label for="star5" title="text">5 stars</label><input type="radio" id="star4" name="rate" value="4" class="KEY" /><label for="star4" title="text">4 stars</label><input type="radio" id="star3" name="rate" value="3" class="KEY" /><label for="star3" title="text">3 stars</label><input type="radio" id="star2" name="rate" value="2" class="KEY" /><label for="star2" title="text">2 stars</label><input type="radio" id="star1" name="rate" value="1" class="KEY" /><label for="star1" title="text">1 star</label></div><div class="col-sm-12"></div><div class="col-sm-12"><textarea class="form-control" placeholder="Description" id="des__KEY"></textarea></div><div class="row col-sm-12"><div class="col-sm-10"></div><div class="col-sm-2"><hr><button type="button" class="btn btn-primary" onclick="add_rate(this)" data-id="KEY">Submit</button></div></div></div>';
+var templates = {"rating": ["Ratings", rating_template]};
 
 function get_template(type, key) {
     var mod_template = templates[type];
-    mod_template[1] = mod_template[1].replace(/KEY/g, key);
+    mod_template[1] = rating_template.replace(/KEY/g, key);
     return mod_template;
 }
 
