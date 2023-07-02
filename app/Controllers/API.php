f820e5bb31c0008310cd99f41b6cebcb9d12716d
danuja-dilanka <dilanka.rajapakshe1@gmail.com>
2023-07-02 12:50:05 +0530

    Update API.php

--- app/Controllers/API.php
+++ app/Controllers/API.php
@@ -339,8 +339,8 @@ class API extends BaseController {
         if ($this->request->is('post') && already_logined()) {
             $Common_model = model("Common_model");
             for ($i = 1; $i <= 12; $i++) {
-                $deposit[$i - 1] += $Common_model->get_data("SELECT SUM(repay_amount) AS tot_repay_amount FROM `" . DB_PREFIX . "loan_pay` WHERE `pay_date` LIKE '%" . sprintf('%02d', $i) . "%'")->tot_repay_amount;
-                $withdraw[$i - 1] += $Common_model->get_data("SELECT SUM(c.last_amount) AS tot_last_amount FROM `" . DB_PREFIX . "_loan_release` a INNER JOIN `" . DB_PREFIX . "loan_request` b ON a.loan = b.id INNER JOIN `" . DB_PREFIX . "loan_product` c ON b.loan_type = c.id WHERE a.rel_date LIKE '%" . sprintf('%02d', $i) . "%'")->tot_last_amount;
+                $deposit[$i - 1] += floatval($Common_model->get_data("SELECT SUM(repay_amount) AS tot_repay_amount FROM `" . DB_PREFIX . "loan_pay` WHERE `pay_date` LIKE '%-" . sprintf('%02d', $i) . "%-'")->tot_repay_amount);
+                $withdraw[$i - 1] += floatval($Common_model->get_data("SELECT SUM(c.last_amount) AS tot_last_amount FROM `" . DB_PREFIX . "loan_release` a INNER JOIN `" . DB_PREFIX . "loan_request` b ON a.loan = b.id INNER JOIN `" . DB_PREFIX . "loan_product` c ON b.loan_type = c.id WHERE a.rel_date LIKE '%-" . sprintf('%02d', $i) . "-%'")->tot_last_amount);
             }
         }
 
