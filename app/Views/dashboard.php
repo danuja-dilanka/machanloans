30e08e181d6075f5f7071938fd6c12af9b520ea5
danuja-dilanka <dilanka.rajapakshe1@gmail.com>
2023-07-02 19:08:21 +0530

    Update dashboard.php

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -140,48 +140,21 @@
         </ul>
         <div class="tab-content" id="pills-tabContent">
             <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
-                <div class="col-lg-12">
-                    <div class="card mb-4">
-                        <div class="card-header">
-                            Recent Transactions
-                        </div>
-                        <div class="card-body">
-                            <?= view('_reports/_sections/transaction_tb') ?>
-                        </div>
-                    </div>
-                </div>
+                <?= view('_reports/_sections/transaction_tb') ?>
             </div>
             <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
-                <div class="col-lg-12">
-                    <div class="card mb-4">
-                        <div class="card-header">
-                            Due Payments
-                        </div>
-                        <div class="card-body">
-                            <?= view('_reports/_sections/due_pay_tb') ?>
-                        </div>
-                    </div>
-                </div>
+                <?= view('_reports/_sections/due_pay_tb') ?>
             </div>
             <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">
-                <div class="col-lg-12">
-                    <div class="card mb-4">
-                        <div class="card-header">
-                            Repayments
-                        </div>
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-sm-5">
-                                    <button type="button" id="repay_date" data-tb='repay_tb' data-unic="1" data-id="<?= $today = date("Y-m-d") ?>" data-type="0" class="filter btn btn-default" style="border: none;">Today</button>&nbsp;
-                                    <button type="button" id="repay_date" data-tb='repay_tb' data-unic="1" data-id="<?= date('Y-m-d', strtotime("$today +1 day")); ?>" data-type="0" class="filter btn btn-default" style="border: none;">Tomorrow</button>&nbsp;
-                                    <button type="button" id="repay_date" data-tb='repay_tb' data-unic="1" data-id="<?= date('Y-m-d', strtotime("$today +7 days")); ?>" data-type="0" class="filter btn btn-default" style="border: none;">Week</button>&nbsp;
-                                </div>
-                            </div>
-                            <hr>
-                            <?= view('_reports/_sections/repay_tb') ?>
-                        </div>
+                <div class="row">
+                    <div class="col-sm-5">
+                        <button type="button" id="repay_date" data-tb='repay_tb' data-unic="1" data-id="<?= $today = date("Y-m-d") ?>" data-type="0" class="filter btn btn-default" style="border: none;">Today</button>&nbsp;
+                        <button type="button" id="repay_date" data-tb='repay_tb' data-unic="1" data-id="<?= date('Y-m-d', strtotime("$today +1 day")); ?>" data-type="0" class="filter btn btn-default" style="border: none;">Tomorrow</button>&nbsp;
+                        <button type="button" id="repay_date" data-tb='repay_tb' data-unic="1" data-id="<?= date('Y-m-d', strtotime("$today +7 days")); ?>" data-type="0" class="filter btn btn-default" style="border: none;">Week</button>&nbsp;
                     </div>
                 </div>
+                <hr>
+                <?= view('_reports/_sections/repay_tb') ?>
             </div>
         </div>
     </div>
