4d4199d566b79b4c09292d1c52c748f20881b537
danuja-dilanka <dilanka.rajapakshe1@gmail.com>
2023-07-08 16:47:51 +0530

    Update sms_helper.php

--- app/Helpers/sms_helper.php
+++ app/Helpers/sms_helper.php
@@ -4,7 +4,7 @@ function send_sms($to, $message) {
     $token = "ab9874ad49d48134c2ec6823f0f5d7fe83308dcb";
     $mobile = trim($to);
     $encoded = urlencode(utf8_encode($message));
-    
+
     $response = "{}";
     if (strlen($mobile) == 11) {
         $curl = curl_init();
@@ -23,7 +23,7 @@ function send_sms($to, $message) {
         curl_close($curl);
 
         $post_data = json_decode($response);
-        if(!isset($post_data->id)){
+        if (!isset($post_data->id)) {
             $post_data->id = 0;
         }
         model('Sms_model')->add_data([
@@ -33,11 +33,39 @@ function send_sms($to, $message) {
             "mess_id" => $post_data->id,
             "send_dt" => date("Y-m-d H:i:s")
         ]);
-        
-        
-    }else{
+    } else {
         $response = '{"message":"failed", errors":{"phone":["Invalid Phone"]}}';
     }
 
     return json_decode($response);
+}
+
+function make_sms($short_code, $data) {
+    $message = null;
+    $Setting_model = model('Setting_model');
+    $template = $Setting_model->get_sms_template_by(["short_code" => $short_code]);
+    if ($template[0]->id) {
+        $rep_codes = $Setting_model->get_sms_rep_code_by(["template" => $template[0]->id]);
+        $message = $template[0]->template;
+        foreach ($rep_codes as $key => $value) {
+            $message = str_replace($value->short_code, isset($data[$value->short_code]) ? $data[$value->short_code] : "", $message);
+        }
+    }
+    
+    return $message;
+}
+
+function make_and_send_sms($short_code, $data, $phone) {
+    $message = null;
+    $Setting_model = model('Setting_model');
+    $template = $Setting_model->get_sms_template_by(["short_code" => $short_code]);
+    if ($template[0]->id) {
+        $rep_codes = $Setting_model->get_sms_rep_code_by(["template" => $template[0]->id]);
+        $message = $template[0]->template;
+        foreach ($rep_codes as $key => $value) {
+            $message = str_replace($value->short_code, isset($data[$value->short_code]) ? $data[$value->short_code] : "", $message);
+        }
+    }
+    
+    return send_sms($phone, $message);
 }
